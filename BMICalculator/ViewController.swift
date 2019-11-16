d3c09ec74fcaf677efc476db7ad216b867913319
Gavin Daniel <gavindaniel@email.arizona.edu>
2019-11-16 09:50:29 -0800

    initial commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by Gavin Daniel on 11/16/19.
+//  Copyright © 2019 Gavin Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
